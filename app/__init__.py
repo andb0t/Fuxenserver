f7b067aa793a91634356ad2199f7af3e8de3b90f
Andreas Alexander Maier <andbot@gmx.de>
2017-09-17 16:35:33 +0200

    Use external webserver

--- app/__init__.py
+++ app/__init__.py
@@ -8,8 +8,4 @@ app = Flask('foxApp')
 def index():
     return 'Hello from FLASK, my hostname is: %s \n' % (socket.gethostname())
 
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
-
 # http://localhost:5000/
