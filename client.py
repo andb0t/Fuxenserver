4328e82c3b7eb5046ce263f2b8e3ad93096d8878
Andreas Alexander Maier <andbot@gmx.de>
2017-09-17 17:11:43 +0200

    Add messages api

--- client.py
+++ client.py
@@ -1,4 +1,24 @@
 import requests
 
-response = requests.get('http://localhost:5000/')
-print(response.text)
+
+def read_messages():
+    response = requests.get('http://localhost:5000/messages')
+    print('TODO: implement check for json data')
+    print(response.text)
+    print('response.json()    :', response.json())
+    print('response.json()[0] :', response.json()[0])
+
+
+def post_message(username, message):
+    response = requests.post('http://localhost:5000/messages',
+                             json={'message': message,
+                                   'username': username
+                                   }
+                             )
+    # if request fails or throws error
+    response.raise_for_status()
+
+
+read_messages()
+post_message('andb0t', 'Hi, posting message!')
+read_messages()
